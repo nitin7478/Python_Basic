a51ebaaa3c319e204f2d1d84a674a2df40e00360
nitin7478 <nitin7478@gmail.com>
2022-11-05 22:20:55 +0530

    api request

--- api_practice/test.py
+++ api_practice/test.py
@@ -1,5 +1,25 @@
-print("hello")
+from flask import  Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/xyz',methods = ['GET','POST'])
+def test():
+    if(request.method == 'POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a + b
+        return jsonify(str(result))
 
 
 
-print("This is my seconf commit")
+# @app.route('/db_mogo',methods = ['GET','POST'])
+# def mongo_connect():
+#     if(request.method=="POST"):
+#         a = request.json['conn']
+#         x = db.db_m_connect()
+#         return jsonify(str(x))
+
+
+if __name__ == '__main__':
+    app.run()
